0bef7281c7d09506847ad17159895546e30e6331
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-09-30 21:49:38 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,25 @@ fn eval(input: &[Json]) -> Json {
   do_run(input, &mut env)
 }
 
+fn substitute(expr: &Json, env: &mut HashMap<String, i64>) -> i64 {
+  match *expr {
+    Json::Array(ref arr) => do_run(arr.as_slice(), env).as_i64().unwrap(),
+    Json::I64(ref i) => *i,
+    Json::U64(ref i) => *i as i64,
+    _ => panic!("cannot substitute: {:?} (env: {:?})", expr, env)
+  }
+}
+
 #[allow(unreachable_code)]
 fn do_run(input: &[Json], env: &mut HashMap<String, i64>) -> Json {
   match input[0] {
     Json::String(ref s) => {
       match s.as_str() {
         "step" => {
-          // println!("[step] {:?},{:?}", &input[1..], env);
-          let ret = (&input[1..]).iter().map(|line| do_run(line.as_array().unwrap().as_slice(), env)).collect();
+          let ret: Vec<_> = (&input[1..]).iter().map(|line| do_run(line.as_array().unwrap().as_slice(), env)).collect();
           ret.into_iter().last().unwrap()
         }
         "until" => {
-          // println!("[until] {:?},{:?}", &input[1..], env);
           loop {
             let c = do_run(input[1].as_array().unwrap().as_slice(), env);
             if c.is_boolean() && c.as_boolean().unwrap() {
@@ -29,57 +36,23 @@ fn do_run(input: &[Json], env: &mut HashMap<String, i64>) -> Json {
           unreachable!()
         }
         "get" => {
-          // println!("[get] {:?},{:?}", &input[1..], env);
           let key = input[1].as_string().unwrap();
           env.get(key).cloned().map(Json::I64).unwrap()
         }
         "set" => {
-          // println!("[set] {:?},{:?}", &input[1..], env);
           let key = input[1].as_string().unwrap();
-          let val = match input[2] {
-            Json::Array(ref arr) => do_run(arr.as_slice(), env).as_i64().unwrap(),
-            Json::I64(ref i) => *i,
-            Json::U64(ref i) => *i as i64,
-            _ => panic!("hogehoge")
-          };
+          let val = substitute(&input[2], env);
           *env.entry(key.to_owned()).or_insert(0) = val;
           Json::Null
         }
         "=" => {
-          // println!("[=] {:?},{:?}", &input[1..], env);
-          let lhs = match input[1] {
-            Json::Array(ref arr) => do_run(arr.as_slice(), env).as_i64().unwrap(),
-            Json::I64(ref i) => *i,
-            Json::U64(ref i) => *i as i64,
-            _ => panic!("hogehoge")
-          };
-
-          let rhs = match input[2] {
-            Json::Array(ref arr) => do_run(arr.as_slice(), env).as_i64().unwrap(),
-            Json::I64(ref i) => *i,
-            Json::U64(ref i) => *i as i64,
-            _ => panic!("hogehoge")
-          };
-
+          let lhs = substitute(&input[1], env);
+          let rhs = substitute(&input[2], env);
           Json::Boolean(lhs == rhs)
         }
         "+" => {
-          // println!("[+] {:?},{:?}", &input[1..], env);
-
-          let lhs = match input[1] {
-            Json::Array(ref arr) => do_run(arr.as_slice(), env).as_i64().unwrap(),
-            Json::I64(ref i) => *i,
-            Json::U64(ref i) => *i as i64,
-            _ => panic!("hogehoge")
-          };
-
-          let rhs = match input[2] {
-            Json::Array(ref arr) => do_run(arr.as_slice(), env).as_i64().unwrap(),
-            Json::I64(ref i) => *i,
-            Json::U64(ref i) => *i as i64,
-            _ => panic!("hogehoge")
-          };
-
+          let lhs = substitute(&input[1], env);
+          let rhs = substitute(&input[2], env);
           Json::I64(lhs + rhs)
         }
         _ => panic!("invalid operator: {:?}", s)
