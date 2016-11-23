6c6f56d1cd66b33ebf4d202e92d01b9fb4d332a3
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-23 17:50:30 +0900

    refactor

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -1,15 +1,15 @@
-﻿open System
-open FSharp.Data
+open System
 open System.Collections.Generic
 open System.Text.RegularExpressions
+open FSharp.Data
 
-module orelang =
+module Orelang =
   type Expr = JsonValue
 
   let re_replace (p: string) (r: string) (i: string) : string =
     Regex.Replace (i, p, r)
 
-  let transpile_from_string (s: string) =
+  let ParseFromString (s: string) : Expr option =
     s |> (re_replace  ";.*"                     "")
       |> (re_replace @"\(\s*"                   "[")
       |> (re_replace @"\s*\)"                   "]")
@@ -20,37 +20,38 @@ module orelang =
       |> (re_replace @"[+*=](?=[, \]])"         "\"$0\"")
       |> (re_replace  "[a-zA-Z_][a-zA-Z0-9_]*"  "\"$0\"")
       |> JsonValue.Parse
+      |> Some
 
   type Engine () =
-    let mutable env = Dictionary<string, double>()
+    let mutable env = Dictionary<string, Expr>()
 
     member private this.getValue (k: string) =
       match env.TryGetValue(k) with
       | (true, v) -> Some v
       | _         -> None
 
-    member private this.setValue (k: string) (v: double) =
+    member private this.setValue (k: string) (v: Expr) =
       if env.ContainsKey(k) then
         env.Remove(k) |> ignore
       env.Add(k, v)
 
     member private this.substitute (expr: Expr) =
       match expr with
-      | JsonValue.Array arr ->
+      | Expr.Array arr ->
         match this.Evaluate expr with
-        | Some (JsonValue.Float v)  -> Some v
-        | Some (JsonValue.Number v) -> Some (double v)
-        | _                         -> None
-      | JsonValue.Float v   -> Some v
-      | JsonValue.Number v  -> Some (double v)
-      | _                   -> None
- 
+        | Some (Expr.Float v)  -> Some (Expr.Float v)
+        | Some (Expr.Number v) -> Some (Expr.Float (double v))
+        | _                    -> None
+      | Expr.Float v   -> Some (Expr.Float v)
+      | Expr.Number v  -> Some (Expr.Float (double v))
+      | _              -> None
+
     member this.Evaluate (expr: Expr) =
       let arr = expr.AsArray ()
       let token = (Array.head arr).AsString ()
       let args = Array.tail arr
       if args.Length = 0 then
-        (this.getValue token) |> Option.map (fun v -> JsonValue.Float v)
+        this.getValue token
       else
         match token with
         | "step" ->
@@ -64,50 +65,52 @@ module orelang =
         | "until" ->
           let rec eval_until e0 e1 =
             match this.Evaluate e0 with
-            | Some (JsonValue.Boolean true) -> Some JsonValue.Null
+            | Some (Expr.Boolean true) -> Some Expr.Null
             | _ -> this.Evaluate e1 |> ignore
                    eval_until e0 e1
           eval_until (Array.get args 0) (Array.get args 1)
 
         | "set" ->
           let k = (Array.get args 0).AsString ()
-          this.substitute (Array.get args 1)
-          |> Option.map (fun v -> this.setValue k v; JsonValue.Null)
+          this.substitute (Array.get args 1) |> Option.map (fun v -> this.setValue k v; Expr.Null)
 
         | "=" ->
           let lhs = this.substitute (Array.get args 0)
           let rhs = this.substitute (Array.get args 1)
           match (lhs, rhs) with
-          | (Some l, Some r)  -> Some (JsonValue.Boolean (Math.Abs(l - r) < 1e-6))
-          | _                 -> None
+          | (Some (Expr.Float l), Some (Expr.Float r)) -> Some (Expr.Boolean (Math.Abs(l - r) < 1e-6))
+          | _                                          -> None
 
         | "+" ->
           let lhs = this.substitute (Array.get args 0)
           let rhs = this.substitute (Array.get args 1)
           match (lhs, rhs) with
-          | (Some l, Some r)  -> Some (JsonValue.Float (l + r))
-          | _                 -> None
+          | (Some (Expr.Float l), Some (Expr.Float r)) -> Some (Expr.Float (l + r))
+          | _                                          -> None
 
         | _ -> None
 
 
-let source = """
-(step
-  (set i 10)
-  (set sum 0)
-  (until (= (i) 0)
-    (step
-      (set sum (+ (sum) (i)))
-      (set i (+ (i) -1))))
-  (sum)
-)
-"""
-
 [<EntryPoint>]
 let main _ =
-  let parsed = orelang.transpile_from_string source
-  let e = new orelang.Engine ()
-  match e.Evaluate parsed with
-  | Some value -> Console.WriteLine("result: {0}", value)
-  | None       -> Console.WriteLine("failed to parse sources")
+  let source = """
+  (step
+    (set i 10)
+    (set sum 0)
+    (until (= (i) 0)
+      (step
+        (set sum (+ (sum) (i)))
+        (set i (+ (i) -1))))
+    (sum)
+  )
+  """
+
+  let eng = new Orelang.Engine ()
+
+  match Orelang.ParseFromString source with
+  | None         -> Console.WriteLine("failed to parse sources")
+  | Some ast ->
+    match eng.Evaluate ast with
+    | Some value -> Console.WriteLine("computational result = {0}", value)
+    | None       -> Console.WriteLine("failed to complete evaluation.")
   0
