9ca6b87b46415c09d94c7ae054ca2ce294e95e36
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-24 20:34:45 +0000

    nom

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,46 @@
 extern crate rustc_serialize;
+#[macro_use]
+extern crate nom;
 
 mod orelang;
 
+mod parse {
+  use std::str;
+  use nom::{self, alphanumeric};
+
+  #[derive(Debug)]
+  pub enum Expr {
+    Token(String),
+    Array(Vec<Expr>),
+  }
+
+  named!(token<&str>, map_res!(call!(alphanumeric),str::from_utf8));
+
+  named!(array< Vec<Expr> >,
+    delimited!(
+      tag!("("),
+      separated_list!(tag!(" "), expr),
+      tag!(")")
+  )
+);
+
+  named!(expr<Expr>,
+    alt!(
+      array => { |v| Expr::Array(v)               }
+    | token => { |s| Expr::Token(String::from(s)) }
+    )
+  );
+
+  pub fn from_str(s: &str) -> nom::IResult<&[u8], Expr> {
+    expr(s.as_bytes())
+  }
+}
+
+
 fn main() {
+  println!("{:?}", parse::from_str("hoge"));
+  println!("{:?}", parse::from_str("(hoge 1 2 (aa 2 3))"));
+
   let source = r#"
 ["step",
   ["set", "i", 10],
@@ -20,4 +58,5 @@ fn main() {
 
   let mut e = orelang::Engine::new();
   println!("{:?}", e.eval(&input));
+
 }
