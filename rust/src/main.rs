960fb613feb4d5164b84dd6469ed6d7bb72caf55
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-25 16:15:18 +0900

    wip

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,8 +2,6 @@ extern crate rustc_serialize;
 #[macro_use]
 extern crate nom;
 
-mod orelang;
-
 mod parse {
   use std::fs::File;
   use std::io::{self, Read};
@@ -36,8 +34,8 @@ mod parse {
       tag!("("),
       separated_list!(tag!(" "), expr),
       tag!(")")
-  )
-);
+    )
+  );
 
   named!(expr<Expr>,
     alt!(
@@ -65,23 +63,4 @@ fn main() {
   println!("{:?}", parse::from_str("hoge"));
   println!("{:?}", parse::from_str("(hoge 1 2 (aa 2 3))"));
   println!("{:?}", parse::from_file("../examples/example_sum.ore"));
-
-  let source = r#"
-["step",
-  ["set", "i", 10],
-  ["set", "sum", 0],
-  ["until", ["==", ["get", "i"], 0], [
-    "step",
-    ["set", "sum", ["+", ["get", "sum"], ["get", "i"]]],
-    ["set", "i", ["+", ["get", "i"], -1]]
-  ]],
-  ["get", "sum"]
-]
-"#;
-
-  let input = orelang::transpile_from_str(source);
-
-  let mut e = orelang::Engine::new();
-  println!("{:?}", e.eval(&input));
-
 }
