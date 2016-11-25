dad5a41f6c1b09bee10b760103e489e60c27fbd3
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-25 18:11:11 +0900

    wip

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,24 +2,26 @@ extern crate rustc_serialize;
 #[macro_use]
 extern crate nom;
 
+use std::io;
+
+#[derive(Debug)]
+pub enum Error {
+  Io(io::Error),
+  NomParse,
+}
+
+impl From<io::Error> for Error {
+  fn from(err: io::Error) -> Error {
+    Error::Io(err)
+  }
+}
+
+
 mod parse {
   use std::fs::File;
-  use std::io::{self, Read};
+  use std::io::Read;
   use std::str;
   use nom::{self, alphanumeric, multispace};
-
-  #[derive(Debug)]
-  pub enum Error {
-    Io(io::Error),
-    Unknown,
-  }
-
-  impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Error {
-      Error::Io(err)
-    }
-  }
-
 
   #[derive(Debug)]
   pub enum Expr {
@@ -44,20 +46,41 @@ mod parse {
     )
   );
 
-  pub fn from_str(s: &str) -> Result<Expr, Error> {
+  pub fn from_str(s: &str) -> Result<Expr, super::Error> {
     match expr(s.trim().as_bytes()) {
       nom::IResult::Done(_, expr) => Ok(expr),
-      _ => Err(Error::Unknown),
+      _ => Err(super::Error::NomParse),
     }
   }
 
-  pub fn from_file(path: &str) -> Result<Expr, Error> {
+  pub fn from_file(path: &str) -> Result<Expr, super::Error> {
     let mut content = String::new();
     File::open(path)?.read_to_string(&mut content)?;
     from_str(&content)
   }
 }
 
+mod ast {
+  use super::parse::Expr;
+
+  #[derive(Debug)]
+  pub enum Ast {
+    Nil,
+    Symbol(String),
+    Number(i64),
+    Step(Vec<Box<Ast>>),
+    Set(String, Box<Ast>),
+    Until(Box<Ast>, Box<Ast>),
+    Eq(Box<Ast>, Box<Ast>),
+    Plus(Box<Ast>, Box<Ast>),
+    Neg(Box<Ast>),
+    Print(Box<Ast>),
+  }
+
+  pub fn from_expr(e: &Expr) -> Option<Ast> {
+    None
+  }
+}
 
 fn main() {
   println!("{:?}", parse::from_str("hoge"));
