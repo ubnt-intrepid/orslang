0a21977fcc394f49184cd11a794399368cbd1295
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-25 11:40:12 +0000

    fsharp

--- fsharp/orelang2/Program.fs
+++ fsharp/orelang2/Program.fs
@@ -1,4 +1,6 @@
-﻿type ('t, 'e) Result = OK of 't | Error of 'e
+// Program.fs
+
+type ('t, 'e) Result = OK of 't | Error of 'e
 
 module Parse =
   open FParsec
@@ -6,18 +8,40 @@ module Parse =
   open FParsec.CharParsers
   open System.IO
 
-  type ErrorKind = NotImplememted
+  type Expr = Token of string
+            | List of Expr list
 
-  let FromString (s: string) : Result<string, ErrorKind> =
-    Result.Error ErrorKind.NotImplememted
+  type ErrorKind = ParseError of string
 
-  let FromFile (path: string) : Result<string, ErrorKind> =
+  type ExprParser = Parser<Expr, unit>
+
+  let private expr, exprRef = createParserForwardedToRef<Expr, unit>()
+
+  let private token : ExprParser = regex "[a-zA-Z0-9\+\-\*\/\=\!]+" |>> Token
+
+  let private plist =
+    between <| pstring "("
+            <| pstring ")"
+            <| (sepEndBy expr spaces) |>> Expr.List
+
+  do exprRef := choice [token
+                        plist]
+
+  let private parseFromString s =
+    run (spaces >>. expr .>> spaces .>> eof) s
+
+  let FromString (s: string) : Result<Expr, ErrorKind> =
+    match parseFromString s with
+    | Success (r, _, _)   -> OK r
+    | Failure (msg, _, _) -> Result.Error <| ParseError msg
+
+  let FromFile (path: string) : Result<Expr, ErrorKind> =
     FromString <| File.ReadAllText path
 
 [<EntryPoint>]
 let main _ =
   let test_string s =
-    printfn "simple: %A" <| Parse.FromString s
+    printfn "string: %A" <| Parse.FromString s
 
   let test_file path =
     printfn "%s: %A" <| System.IO.Path.GetFileName path <| Parse.FromFile path
