0629446709986e4f679b96346a529e0743aa6b7b
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-27 15:43:25 +0000

    scala: impl parser + refactor

--- scala/Main.scala
+++ scala/Main.scala
@@ -16,7 +16,8 @@ object ExprParser extends RegexParsers {
     "nil".r ~> success(Nil())
 
   def _boolean: Parser[Expr] =
-    "true".r ~> success(Bool(true)) | "false".r ~> success(Bool(false))
+    "true".r  ~> success(Bool(true)) |
+    "false".r ~> success(Bool(false))
 
   def _number: Parser[Expr] =
     """[\+\-]?[1-9][0-9]+""".r ^^ { case (s) => Number(s.toInt) }
@@ -25,25 +26,30 @@ object ExprParser extends RegexParsers {
     """[a-zA-Z0-9\+\-\*\/\=\!]+""".r ^^ { Symbol(_) }
 
   def _function: Parser[Expr] =
-    ws ~> "(" ~> ws ~> _symbol <~ ws <~ repsep(_expr, """[ \t\n]+""".r) <~ ")" <~ ws
+    "(" ~> ws ~> """[a-zA-Z0-9\+\-\*\/\=\!]+""".r ~ ws ~ repsep(_expr, ws) <~ ")" ^^ {
+      case (name ~ ws ~ args) => Function(name, args)
+    }
 
   def _expr: Parser[Expr] =
     ws ~> (_nil | _boolean | _number | _symbol | _function) <~ ws
 
-  def apply(str: String): Either[String, Expr] =
+  def from_str(str: String): Either[String, Expr] =
     parseAll(_expr, str) match {
-      case Success(p, next)     => Right(p)
-      case NoSuccess(msg, next) => Left(s"$msg on line ${next.pos.line} on column ${next.pos.column}")
+      case Success(parsed, next) => Right(parsed)
+      case NoSuccess(msg, next)  => Left(s"$msg on line ${next.pos.line} on column ${next.pos.column}")
     }
+
+  def from_file(path: String): Either[String, Expr] =
+    from_str(Source.fromFile(path).mkString)
 }
 
 object Main {
   def main(args: Array[String]) = {
-    println(ExprParser("nil"))
-    println(ExprParser("false"))
-    println(ExprParser("-10"))
-    println(ExprParser("hoge"))
-    println(ExprParser("(+ 1 (+ sum -1))"))
-    println(ExprParser(Source.fromFile("../examples/example_sum.ore").mkString))
+    println(ExprParser.from_str("nil"))
+    println(ExprParser.from_str("false"))
+    println(ExprParser.from_str("-10"))
+    println(ExprParser.from_str("hoge"))
+    println(ExprParser.from_str("(+ 1 (+ sum -1))"))
+    println(ExprParser.from_file("../examples/example_sum.ore"))
   }
 }
