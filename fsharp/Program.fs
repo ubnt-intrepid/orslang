88f4ac37dfe97582391be5c8564cdacdc716ac6e
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-28 02:16:49 +0900

    fsharp: impl engine

--- fsharp/Program.fs
+++ fsharp/Program.fs
@@ -8,7 +8,7 @@ module Orelang =
   type Expr = Nil
             | Number of decimal
             | Symbol of string
-            | Command of string * Expr list
+            | Function of string * Expr list
 
   type ErrorKind = ParseError of string
 
@@ -31,7 +31,7 @@ module Orelang =
 
     let private command expr =
       pstring "(" >>. spaces >>. symbol .>> spaces .>>. sepEndBy expr spaces .>> pstring ")"
-      |>> fun (symbol, args) -> Command (symbol, args)
+      |>> fun (symbol, args) -> Function (symbol, args)
 
     let private expr =
       fix <| fun expr -> token <|> command expr
@@ -48,9 +48,28 @@ module Orelang =
   let ParseFromFile (path: string) : Result<Expr, ErrorKind> =
     ParseFromString <| System.IO.File.ReadAllText path
 
+  type Engine() =
+    let env = new System.Collections.Generic.Dictionary<string, Expr>()
+
+    member private this.getValue k =
+      match env.TryGetValue(k) with
+      | (true, v) -> Some v
+      | _         -> None
+
+    member private this.setValue k v =
+      env.Remove(k) |> ignore
+      env.Add(k, v)
+
+    member this.Evaluate (expr: Expr) : Expr option =
+      Some expr
+
+
+open System.IO
 
 [<EntryPoint>]
 let main _ =
+  let repo_root = Directory.GetParent(__SOURCE_DIRECTORY__).ToString()
+  
   let test_string s =
     match Orelang.ParseFromString s with
     | Result.OK ast -> printfn "string:\n%A\n" ast
@@ -63,7 +82,16 @@ let main _ =
 
   test_string   "nil"
   test_string   "(+ 1 2 (* 3 4))"
-  test_file     "../examples/example_sum.ore"
+  test_file  <| repo_root + "/examples/example_sum.ore"
 
-  test_string   "(+ 1 2 (* 3 4)))"
+  match Orelang.ParseFromFile <| repo_root + "/examples/example_sum.ore" with
+  | Result.Error msg ->
+    printfn "failed to parse: %A\n" msg
+  | Result.OK expr ->
+    let engine = Orelang.Engine()
+    let result = engine.Evaluate expr
+    printfn "result: %A" result
+
+  // failure case
+  // test_string   "(+ 1 2 (* 3 4)))"
   0
