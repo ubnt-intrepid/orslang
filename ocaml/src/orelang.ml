19fe1541dc89fd2df085457237ed702caaa6ae6a
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-27 06:35:56 +0000

    ocaml

--- ocaml/src/orelang.ml
+++ ocaml/src/orelang.ml
@@ -10,6 +10,7 @@ type expr =
   | Function of { name: string; args: expr list; }
 [@@deriving show]
 
+
 let spaces = skip_while (function
     | ' ' | '\n' | '\t' -> true
     | _ -> false)
@@ -19,34 +20,41 @@ let rparen = string ")"
 
 let ws = spaces
 
-let symbol = take_while1 (function
+let _symbol = take_while1 (function
     | 'a'..'z' | 'A'..'Z' | '0'..'9'
     | '+' | '-' | '*' | '/' | '=' -> true
     | _ -> false)
 
+
 let nil =
   string "nil" *> return Nil
 
-let _bool =
+let boolean =
   string "true" *> return (Bool true)
   <|>
   string "false" *> return (Bool false)
 
-let num_or_sym =
-  let to_expr s = try Number (int_of_string s) with | _ -> Symbol s in
-  symbol >>| to_expr
+let number = _symbol
+  >>= (fun s ->
+      try return (Number (int_of_string s))
+      with | _ -> fail "number")
 
-let command expr =
-  lparen *> ws *> lift2 (fun s a -> Function { name = s; args = a; }) (symbol <* ws) (sep_by ws expr) <* rparen
+let symbol =
+  _symbol >>| fun s -> Symbol s
+
+let _function expr =
+  let _function s a = Function { name = s;
+                                 args = a; } in
+  lparen *> ws *> lift2 _function (_symbol <* ws) (sep_by ws expr) <* rparen
 
 let expr = fix (fun expr ->
-    let token =
+    ws *> (
       nil
-      <|> _bool
-      <|> num_or_sym
-      <|> command expr
-    in
-    ws *> token <* ws)
+      <|> boolean
+      <|> number
+      <|> symbol
+      <|> _function expr
+    ) <* ws)
 
 let parse_from_str s : expr option =
   match parse_only expr (`String s) with
