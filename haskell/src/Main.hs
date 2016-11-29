12118158332a50e08de9cb06c64c5ea1175003f5
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-29 18:19:42 +0000

    simple parsec test

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,3 +1,24 @@
-import Text.ParserCombinators.Parsec
+{- Main.hs  -}
+import           Control.Applicative ( (*>), (<$>), (<*) )
+import           Data.Char           ( ord )
+import           Text.Parsec         ( (<|>), char, oneOf, parseTest )
+import           Text.Parsec.String  ( Parser )
 
-main = putStrLn "Hello"
+data Exp = Add Exp Exp
+         | Mul Exp Exp
+         | Nat Int
+    deriving Show
+
+expr = Add <$> term <$> (char '+' *> expr) <|> term
+
+term = Mul <$> factor <$> (char '*' *> term) <|> factor
+
+factor = (char '(' *> expr <* char ')') <|> nat
+
+nat = Nat . charToInt <$> oneOf ['0' .. '9']
+  where
+    charToInt c = ord c - ord '0'
+
+main = do
+    parseTest expr "1+2*3"
+    parseTest expr "(1+2)*3"
