5e4f987a23d3f4c17500f6c561196ee46fd6721b
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2016-11-30 07:17:45 +0000

    scala: update parser

--- scala/Main.scala
+++ scala/Main.scala
@@ -2,41 +2,41 @@ import scala.util.parsing.combinator._
 import scala.io.Source
 
 sealed abstract trait Expr
-case object Nil extends Expr
 case class Bool(b: Boolean) extends Expr
 case class Number(n: Int) extends Expr
 case class Symbol(s: String) extends Expr
-case class Function(name: String, args: Seq[Expr]) extends Expr
+case class List(lst: Seq[Expr]) extends Expr
 
 
 object ExprParser extends RegexParsers {
-  def ws = """[ \t\n]*""".r
+  override val skipWhitespace = false
 
-  def _nil: Parser[Expr] =
-    "nil".r ~> success(Nil)
+  def ws = """[ \t\n]*""".r
+  def ws1 = """[ \t\n]+""".r
 
   def _boolean: Parser[Expr] =
     "true".r  ~> success(Bool(true)) |
     "false".r ~> success(Bool(false))
 
   def _number: Parser[Expr] =
-    """[\+\-]?[1-9][0-9]+""".r ^^ { case (s) => Number(s.toInt) }
+    """[\+\-]?[0-9]+""".r ^^ { case (s) => Number(s.toInt) }
 
   def _symbol: Parser[Expr] =
-    """[a-zA-Z0-9\+\-\*\/\=\!]+""".r ^^ { Symbol(_) }
+    """[a-zA-Z\+\-\*\/=!_][a-zA-Z0-9\+\-\*\/=!_]*""".r ^^ { Symbol(_) }
 
-  def _function: Parser[Expr] =
-    "(" ~> ws ~> """[a-zA-Z0-9\+\-\*\/\=\!]+""".r ~ ws ~ repsep(_expr, ws) <~ ")" ^^ {
-      case (name ~ ws ~ args) => Function(name, args)
-    }
+  def _list: Parser[Expr] =
+    "(" ~> ws ~> repsep(_expr, ws1) <~ ws <~ ")" ^^ { List(_) }
 
   def _expr: Parser[Expr] =
-    ws ~> (_nil | _boolean | _number | _symbol | _function) <~ ws
+    _boolean | _number | _symbol | _list
+
+  def _program: Parser[Expr] =
+    ws ~> _expr <~ ws
 
   def from_str(str: String): Either[String, Expr] =
-    parseAll(_expr, str) match {
+    parseAll(_program, str) match {
       case Success(parsed, next) => Right(parsed)
-      case NoSuccess(msg, next)  => Left(s"$msg on line ${next.pos.line} on column ${next.pos.column}")
+      case NoSuccess(msg, next)  => Left(s"$msg on line ${next.pos.line} on column ${next.pos.column}, ${str}")
     }
 
   def from_file(path: String): Either[String, Expr] =
@@ -46,7 +46,6 @@ object ExprParser extends RegexParsers {
 
 object Main {
   def main(args: Array[String]) = {
-    println(ExprParser.from_str("nil"))
     println(ExprParser.from_str("false"))
     println(ExprParser.from_str("-10"))
     println(ExprParser.from_str("hoge"))
